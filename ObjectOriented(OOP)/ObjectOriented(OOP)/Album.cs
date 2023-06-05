7bb84ddf2fd38e5700a366846661f5ea0d3415f7
Guilherme Buzati <guilherme.leopoldino@buttinimoraes.com.br>
2023-06-05 11:34:42 -0300

    feat(OOP): Course - E4S3 - Showing list of musics

--- ObjectOriented(OOP)/ObjectOriented(OOP)/Album.cs
+++ ObjectOriented(OOP)/ObjectOriented(OOP)/Album.cs
@@ -3,11 +3,22 @@
     private List<Music> musics = new List<Music>();
     public string Name { get; set; }
 
-    public int TotalDuration { get; set; } 
+    public int TotalDuration => musics.Sum(m => m.Duration);
     public void AddMusic(Music music)
     {
 
         musics.Add(music);
 
+    }
+
+    public void ShowMusicsInAlbum()
+    {
+        Console.WriteLine($"List of musics - {Name}\n");
+        foreach (Music music in musics)
+        {
+            Console.WriteLine($"Music: {music.Name}");
+        }
+
+        Console.WriteLine($"Total duration {TotalDuration}");
     }
 }
--- ObjectOriented(OOP)/ObjectOriented(OOP)/Program.cs
+++ ObjectOriented(OOP)/ObjectOriented(OOP)/Program.cs
@@ -3,10 +3,13 @@ albumColdplay.Name = "Music of the Spheres";
 
 Music music1 = new Music();
 music1.Name = "High Power";
+music1.Duration = 230;
 
 Music music2 = new Music();
 music2.Name = "My Universe";
-
+music2.Duration = 223;
 
 albumColdplay.AddMusic(music1);
 albumColdplay.AddMusic(music2);
+
+albumColdplay.ShowMusicsInAlbum();
