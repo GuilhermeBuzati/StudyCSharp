8ef9024380de2df32a723f02d984739f2a5d47c1
Guilherme Buzati <guilherme.leopoldino@buttinimoraes.com.br>
2023-06-06 14:48:29 -0300

    feat(Arrays): Course - E4S1 - First Array

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] ages = new int[5];
+
+ages[0] = 30;
+ages[1] = 40;
+ages[2] = 17;
+ages[3] = 18;
+ages[4] = 21;
+
+Console.WriteLine($"Length array {ages.Length}");
+
+
+int totalAge = 0;
+for(int i = 0; i < ages.Length; i++)
+{
+    int age = ages[i];
+    Console.WriteLine($"Index [{i}] = {age}");
+
+    totalAge += age;
+}
+
+int average = totalAge / ages.Length;
+
+Console.WriteLine($"Average age {average}");
