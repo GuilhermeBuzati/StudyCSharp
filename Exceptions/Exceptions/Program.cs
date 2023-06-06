c4338948c6e10c1840607740a56e07cb9735629a
Guilherme Buzati <guilherme.leopoldino@buttinimoraes.com.br>
2023-06-06 11:24:42 -0300

    feat(OOP): Course - E7S1 - Treatment Error

---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,14 @@
+﻿double TaxOperation = 0;
+int TotalAccount = 0;
+
+
+try
+{
+    TaxOperation = 30 / TotalAccount;
+}
+catch(DivideByZeroException) {
+    Console.WriteLine("Error: Divided by zero!");
+}
+
+
+Console.WriteLine(TaxOperation);
