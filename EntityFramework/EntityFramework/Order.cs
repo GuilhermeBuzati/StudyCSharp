d68e45256c4ed57a4fcee014473f207cf42777de
Guilherme Leopoldino <guilherme.leopoldino@buttinimoraes.com.br>
2023-06-14 17:25:41 -0300

    feat(Entity): Course - E1S5 - Relation 1:N

---
+++ EntityFramework/EntityFramework/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace EntityFramework
+{
+    internal class Order
+    {
+        public Order()
+        {
+        }
+
+        public int Quantity { get; internal set; }
+        public Product Product { get; internal set; }
+    }
+}
--- EntityFramework/EntityFramework/Program.cs
+++ EntityFramework/EntityFramework/Program.cs
@@ -7,7 +7,18 @@ namespace EntityFramework
     {
         static void Main(string[] args)
         {
-            
+
+            var bread = new Product();
+            bread.Name = "Bread";
+            bread.CostUnit = 0.25;
+            bread.Unit = "UN";
+            bread.Category = "Bakery";
+
+            var order = new Order();
+            order.Quantity = 6;
+            order.Product = bread;
+            order.Value = bread.CostUnit * order.Quantity;
+
 
 
         }
