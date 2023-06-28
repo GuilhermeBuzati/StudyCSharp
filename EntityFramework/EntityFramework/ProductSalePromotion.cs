ec7eba00bb8a4e99dcb9b1c3cda573877782a7b1
Guilherme Leopoldino <guilherme.leopoldino@buttinimoraes.com.br>
2023-06-28 09:27:05 -0300

    refactor(Entity): Code not commited

---
+++ EntityFramework/EntityFramework/ProductSalePromotion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+    public class ProductSalePromotion
+    {
+        public int ProductsId { get; set; }
+        public int SalePromotionsId { get; set; }
+        public Product Product { get; set; }
+        public SalePromotion SalePromotion { get; set; }
+    }
+}
